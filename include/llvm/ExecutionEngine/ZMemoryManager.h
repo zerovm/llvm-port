7f8e4d93cff889b10ae66fbff7c66d3edb3199c2
rampage644 <rampage644@gmail.com>
2013-07-23 17:58:27 +0300

    ZMemoryManager changed.
    Implemented apply/resetPermissions methods, modified allocation required size calculation.

--- include/llvm/ExecutionEngine/ZMemoryManager.h
+++ include/llvm/ExecutionEngine/ZMemoryManager.h
@@ -41,6 +41,27 @@ public:
   /// a default alignment of 16 will be used.
   virtual uint8_t* allocateCodeSection(uintptr_t Size, unsigned Alignment,
                                        unsigned SectionID);
+
+  /// \brief Applies section-specific memory permissions.
+  ///
+  /// This method is called when object loading is complete and section page
+  /// permissions can be applied.  It is up to the memory manager implementation
+  /// to decide whether or not to act on this method.  The memory manager will
+  /// typically allocate all sections as read-write and then apply specific
+  /// permissions when this method is called.  Code sections cannot be executed
+  /// until this function has been called.
+  ///
+  /// \returns true if an error occurred, false otherwise.
+  virtual bool applyPermissions(std::string *ErrMsg = 0);
+
+  /// \brief Resets section-specific memory permissions.
+  ///
+  /// This method is called when compiling new function after applyPermissions()
+  /// was called. Memory manager will reset permissions to read-write, so it
+  /// would be possible to JIT new functions.
+  ///
+  /// \returns true if an error occurred, false otherwise.
+  virtual bool resetPermissions(std::string *ErrMsg = 0);
 private:
   OwningPtr<ZCodeMemoryAllocator> AllocatorHelper;
 };
--- lib/ExecutionEngine/MCJIT/ZMemoryManager.cpp
+++ lib/ExecutionEngine/MCJIT/ZMemoryManager.cpp
@@ -2,6 +2,8 @@
 
 #include <vector>
 
+#include "llvm/Support/Memory.h"
+
 namespace llvm {
 
 
@@ -14,7 +16,6 @@ namespace llvm {
 /// It is suited for ZeroVM jail system call. Thus we have a set of memory blocks
 /// with code data with proper alignment.
 ///
-/// TODO: reimplement applyPermissions(), resetPermissions()
 /// TODO: consider memory blocks release (do we need that?)
 class ZCodeMemoryAllocator {
 public:
@@ -33,7 +34,9 @@ public:
     if (!Alignment)
       Alignment = 16;
 
-    uintptr_t RequiredSize = Alignment * ((Size + Alignment - 1)/Alignment + 1);
+    uintptr_t RequiredSize = (Size % Alignment) ?
+                               Alignment * ((Size + Alignment - 1)/Alignment + 1):
+                               Size;
     uint8_t* start = NULL;
 
     if (ZCodeSlabs.empty() ||
@@ -44,22 +47,39 @@ public:
       if (!mb.base())
         return start;
       ZCodeSlabs.push_back(mb);
+      ZCodeSlabsEnd.push_back((uintptr_t)mb.base());
 
 
       // find first 64K alignment
-      start = (uint8_t*)((uintptr_t)(mb.base() + PageAlignment - 1) & ~(uintptr_t)(PageAlignment - 1));
+      start = (uint8_t*)(((uintptr_t)mb.base() + PageAlignment - 1) & ~(uintptr_t)(PageAlignment - 1));
     } else {
       start = FreeSpaceStart;
     }
 
     FreeSpaceStart = start + RequiredSize;
+    ZCodeSlabsEnd.back() = (uintptr_t)FreeSpaceStart;
     start = (uint8_t*)((uintptr_t)(start + Alignment - 1) & ~(uintptr_t)(Alignment - 1));
 
     return start;
   }
 
+  /// Sets allocated memory ready for execution
+  bool setMemoryExecutable() {
+    return applyMemoryPermissions(sys::Memory::MF_READ | sys::Memory::MF_EXEC);
+  }
+  /// Sets allocated memory ready for write
+  bool setMemoryWritable() {
+    return applyMemoryPermissions(sys::Memory::MF_READ | sys::Memory::MF_WRITE);
+  }
 
 private:
+  /// \brief Allocating code slab
+  ///
+  /// Try to allocate memory block of given size. On failure it will return
+  /// non-valid memory block: base address and size will be null
+  ///
+  /// \p size size of memory block to allocate
+  /// \returns allocated memory block
   sys::MemoryBlock allocateNewZSlab(size_t size) {
     error_code ec;
     sys::MemoryBlock* Near = ZCodeSlabs.empty() ? 0 : &ZCodeSlabs.back();
@@ -71,10 +91,36 @@ private:
     return MB;
   }
 
+  /// \brief Apply permissions to allocated memory
+  ///
+  /// Try to apply given permissions to memory we've already allocated. It will
+  /// return false on succes (due to llvm semantics, it also return false on
+  /// succesfull permission apply).
+  /// TODO: apply permissions only to allocated memory (exclude non-usable
+  /// chunks of code)
+  ///
+  /// \p Permissions permissions to apply
+  /// \returns false on success
+  bool applyMemoryPermissions(unsigned int Permissions) {
+    error_code ec;
+    for (unsigned i=0;i<ZCodeSlabs.size();++i) {
+      uint8_t* base = (uint8_t*)((((uintptr_t)ZCodeSlabs[i].base() + PageAlignment - 1) & ~(uintptr_t)(PageAlignment - 1)));
+      size_t size = ZCodeSlabsEnd[i] - (uintptr_t)base;
+      ec = sys::Memory::protectMappedMemory(sys::MemoryBlock(base, size),
+                                            Permissions);
+      if (ec)
+        return true;
+    }
+
+    return false;
+  }
+
 
   /// vector of allocated memory blocks
   /// can't track client allocations inside them
   std::vector<sys::MemoryBlock> ZCodeSlabs;
+  std::vector<uintptr_t>        ZCodeSlabsEnd;
+
 
   /// free space start in last memory block
   uint8_t*    FreeSpaceStart;
@@ -93,4 +139,22 @@ ZMemoryManager::~ZMemoryManager() {
 uint8_t* ZMemoryManager::allocateCodeSection(uintptr_t Size, unsigned Alignment, unsigned SectionID) {
   return AllocatorHelper->allocate(Size, Alignment);
 }
+
+bool ZMemoryManager::applyPermissions(std::string* ErrMsg)
+{
+  // apply permissions
+  bool ret = SectionMemoryManager::applyPermissions(ErrMsg);
+  if (ret)
+    return ret;
+  return AllocatorHelper->setMemoryExecutable();
+}
+
+bool ZMemoryManager::resetPermissions(std::string* ErrMsg)
+{
+  bool ret = SectionMemoryManager::resetPermissions(ErrMsg);
+  if (ret)
+    return ret;
+  return AllocatorHelper->setMemoryWritable();
+}
+
 }
