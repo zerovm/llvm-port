25f2e27ed47a4e41a2129efa3fa7326bf0e45848
Sergei Turukin <rampage644@gmail.com>
2013-09-11 16:16:12 +0300

    configure prefix is set to ZVM_PREFIX

--- build.sh
+++ build.sh
@@ -10,9 +10,10 @@ cd $BUILD_DIR
 
 ../configure --host=x86_64-nacl --target=x86_64-nacl \
 --enable-static --disable-shared \
+--prefix=$ZVM_PREFIX/x86_64-nacl \
 --enable-optimized=yes --enable-pic=yes \
 --disable-pthreads --disable-threads \
 --enable-jit=yes --enable-targets=x86,x86_64,cpp \
 --disable-assertions --with-gcc-toolchain=${ZVM_PREFIX}/x86_64-nacl
 
-CPPFLAGS='-O0' make -j9
+CPPFLAGS='-O0' make -j4
